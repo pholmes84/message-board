45f30ade175bceaa72afe302353760d68c44e984
pholmes84 <theviolist57@gmail.com>
2021-01-03 00:42:04 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  let m = req.body;
+  messages.push({ text: m.message, user: m.name, added: new Date() });
+  res.redirect("/");
+});
+
+module.exports = router;
